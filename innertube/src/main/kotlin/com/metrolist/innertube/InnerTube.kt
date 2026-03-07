565915a6392ede53d97573c00e3c7f19928f0a31
Nao <164814887+naolad@users.noreply.github.com>
2026-03-08 01:58:48 +0900

    Optimize Subscriber count on About in Japanese

--- innertube/src/main/kotlin/com/metrolist/innertube/InnerTube.kt
+++ innertube/src/main/kotlin/com/metrolist/innertube/InnerTube.kt
@@ -852,7 +852,7 @@ class InnerTube {
                         ?.owner
                         ?.videoOwnerRenderer
                         ?.subscriberCountText
-                        ?.simpleText?.split(" ")?.firstOrNull(),
+                        ?.simpleText?.split(" ")?.firstOrNull { part -> part.any { it.isDigit() } },
                 uploadDate = baseForTitle?.dateText?.simpleText,
                 viewCount = returnYouTubeDislikeResponse.viewCount,
                 like = returnYouTubeDislikeResponse.likes,
